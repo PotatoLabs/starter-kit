571934a596d3498954f4f0ea36f1863450ab9571
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-02-18 23:29:16 -0500

    installs eslint + prettier config

---
+++ api/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  plugins: ['react', 'prettier'],
+  rules: {
+    'import/prefer-default-export': ['off'],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_*' }],
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+    'react/display-name': ['off'],
+    'react/prop-types': ['off'],
+    'react/jsx-filename-extension': ['off'],
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+}
